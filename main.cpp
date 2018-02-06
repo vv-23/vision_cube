8485f35bc8ceaea2aa0b0bc172351fcc46715462
Vi Vuong <vuongkyvi@gmail.com>
2018-02-05 16:11:52 -0800

    Added exposure control

--- main.cpp
+++ main.cpp
@@ -1,43 +1,51 @@
-#include "Cube.h"
+#include "opencv2/opencv.hpp"
+#include <cstdlib>
+#include "params_parser.h"
+#include <libv4l2.h>
+#include <linux/videodev2.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
 #include <iostream>
+#include "Cube.h"
+using namespace cv;
+
 
 
 int main(int argc, char **argv) {
-    char *fn  = argv[1];
-    while (1) {
-      cv::VideoCapture capture(fn);
-      while(capture.get(CV_CAP_PROP_POS_FRAMES)<capture.get(CV_CAP_PROP_FRAME_COUNT)-1) {
-        //we can loop the video by re-opening the capture every time the video reaches its last frame
+    char *params_file  = argv[1];                 //Might needs improvement
+    double exposure = std::atof(argv[2]);
+    int descriptor = v4l2_open("/dev/video0", O_RDWR);
+    v4l2_control c;
+  c.id = V4L2_CID_EXPOSURE_AUTO;
+  c.value = 1;	
+  if(v4l2_ioctl(descriptor, VIDIOC_S_CTRL, &c) == 0)
+    std::cout << "success";
+  c.id = V4L2_CID_EXPOSURE_AUTO_PRIORITY;
+  c.value = 0;
+  if (v4l2_ioctl(descriptor, VIDIOC_S_CTRL, &c) == 0)
+    std::cout << "success";
 
+  c.id = V4L2_CID_EXPOSURE_ABSOLUTE;
+  c.value = exposure;
+  if (v4l2_ioctl(descriptor, VIDIOC_S_CTRL, &c) == 0)
+    std::cout << "success";
+    cv::VideoCapture capture(0);
+    while (1) {
         if (!capture.isOpened()) {
           std::cout << "ERROR ACQUIRING VIDEO FEED\n";
           getchar();
           return -1;
         }
-        //check if the video has reach its last frame.
-        //we add '-1' because we are reading two frames from the video at a time.
-        //if this is not included, we get a memory error!
         cv::Mat frame;
         //read first frame
         capture >> frame;
-        if (frame.empty()) {
-          capture.release();
-          capture = cv::VideoCapture(fn);    
-        }
-          
-        Cube cube(frame);
+        Cube cube(frame, params_file);
         cube.getPosition();
         cv::imshow("Cube", cube.showFrame());
         cv::imshow("Camera", frame);
         //cv::imshow("Video", frame);
         cv::waitKey(10);
-        
-      }
-      capture.release();
-    }
-	
-
-
-  
+    };
+    capture.release();
     return 0;
 }
